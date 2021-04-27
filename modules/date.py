ac810427acfa6a3b5bbab6926e31a074dde02d0d
Madaspe <danil.treshchev@yandex.ru>
2021-04-27 17:25:55 +0000

    add simple date module

--- modules/date.py
+++ modules/date.py
@@ -1,1 +1,12 @@
-# TODO
+from time import strftime
+
+
+def get_module_text():
+    # Must have in all module
+    date = strftime("%A, %d. %B %Y %I:%M%p")
+    return date
+
+
+if __name__ == '__main__':
+    # Test
+    print(get_module_text())
