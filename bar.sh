476b6de406dff407084943ffd5a91694c6fb86f5
Madaspe <danil.treshchev@yandex.ru>
2021-05-03 21:35:46 +0000

    Add more logic bash script

--- bar.sh
+++ bar.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+ABSOLUTE_FILEPATH=`readlink -e "$0" | sed "s/\/bar.sh//"`
+
+VENV=`ls $ABSOLUTE_FILEPATH | grep venv`
+
+if [[ $VENV ]]
+then
+	PYTHON_BIN="$ABSOLUTE_FILEPATH/venv/bin/python3"
+
+else 
+	PYTHON_BIN="python3"
+fi
+
+BAR_FILE="$ABSOLUTE_FILEPATH/bar.py"
+
 while true; do
-  BAR_STR=$(/home/madaspe/dwm_python_bar/venv/bin/python3 /home/madaspe/dwm_python_bar/bar.py)
+  BAR_STR=$($PYTHON_BIN $BAR_FILE)
   xsetroot -name "$BAR_STR"
   sleep 1
 done
